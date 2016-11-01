cb4fe1571c409ba14efd74f2dae55ca8c8c2f4b5
Connal de Souza <connaldesouza96@gmail.com>
2016-11-01 19:28:46 -0400

    adding geolocation files

---
+++ geolocation.cpp
@@ -0,0 +1,6 @@
+#include "geolocation.h"
+
+Geolocation::Geolocation()
+{
+
+}
---
+++ geolocation.h
@@ -0,0 +1,11 @@
+#ifndef GEOLOCATION_H
+#define GEOLOCATION_H
+
+
+class Geolocation
+{
+public:
+    Geolocation();
+};
+
+#endif // GEOLOCATION_H
